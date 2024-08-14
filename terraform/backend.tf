16e9ed53a1b6fff15a179c3c3c08388eda0a36c3
Emmanuel Nnamdi <engr.nnamdiemmanuel@gmail.com>
2024-08-14 20:18:12 +0100

    pipe v1.01

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-s3-bucket-name"
-    key            = "terraform/state.tfstate"
-    region         = "us-east-1"
+    bucket         = "your-s3-bucket-name"  # Replace with your actual bucket name
+    key            = "terraform/state"      # Path within the bucket
+    region         = "us-east-1"             # The region where your bucket is located
   }
 }
